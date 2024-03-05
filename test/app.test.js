2dfc461c94cc0383c514243548d438fa053b6d0b
Harcokun <harconsarnan@gmail.com>
2024-03-05 16:37:04 +0700

    Complete admin test flow

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,27 @@
 const request = require("supertest");
-
 const app = require("../server.js");
+const User = require("../models/User");
 const PORT = process.env.PORT || 3000;
 const baseUrl = "127.0.0.1:" + PORT + "/api/v1";
 
-describe("Register, Login, GetMe, Delete Flow", () => {
+describe("Admin Flow", () => {
     let token = "";
     let id = "";
     beforeAll(async () => {
+
+        // Wrong retype password
+        await request(baseUrl)
+            .post("/auth/register")
+            .send({
+                    name: "Test Admin 1",
+                    email: "test_admin1@mail.com",
+                    role: "admin",
+                    password: "admin3939",
+                    retype: "admin"
+                })
+            .expect(422);
+
+        // correct retype password
         await request(baseUrl)
             .post("/auth/register")
             .send({
@@ -35,7 +49,6 @@ describe("Register, Login, GetMe, Delete Flow", () => {
                 expect(res.body).toHaveProperty('token');
                 // Store the token for later use
                 token = res.body.token;
-                console.log(token);
                 done();
             });
     });
@@ -59,16 +72,48 @@ describe("Register, Login, GetMe, Delete Flow", () => {
             });
     });
 
-    it('should delete user', function(done) {
+    it('should get all users', function(done) {
+        request(baseUrl)
+            .get('/user/')
+            .auth(token, {type:'bearer'})
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+
+    it('should update self', function(done) {
+        const updateUserUrl = '/user/' + id;
+        request(baseUrl)
+            .put(updateUserUrl)
+            .auth(token, {type:'bearer'})
+            .send({
+                name: "test admin 39",
+            })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect((res) => {
+                res.body.data.name = "test admin 39";
+            })
+            .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+    });
+
+    it('should delete self', function(done) {
         const deleteUserUrl = '/user/' + id;
         request(baseUrl)
             .delete(deleteUserUrl)
             .auth(token, {type:'bearer'})
             .expect(204)
-            .end(function(err, res) {
-                if (err) return done(err);
+            .then(async () => {
+                expect(await User.findOne({ _id: id })).toBeFalsy();
                 done();
-            });
+            })
+            .catch((err) => done(err));
     });
     
 });
